30e5b9457f92af1d21b1a893faa103e4acc2fab7
jassi1110 <jasnoorsingh11@gmail.com>
2023-09-12 19:03:05 +0530

    Query String range fix : Jasnoor

--- inventory/services.py
+++ inventory/services.py
@@ -12,7 +12,7 @@ def queryString(dict,status):
         if 'maxLength' in dict:
             queryLength &= Q(length__lte=dict['maxLength'])
         
-        if dict['minLength'] and dict['maxLength'] and dict['minLength'] > dict['maxLength']:
+        if 'minLength' in dict and 'maxLength' in dict and dict['minLength'] > dict['maxLength']:
             return None
         query &= queryLength
 
@@ -24,7 +24,7 @@ def queryString(dict,status):
         if 'minBreadth'in dict :
             query &= Q(breadth__gte=dict['minBreadth'])
         
-        if dict['minBreadth'] and dict['maxBreadth'] and dict['minBreadth'] > dict['maxBreadth']:
+        if 'minBreadth' in dict and 'maxBreadth' in dict and dict['minBreadth'] > dict['maxBreadth']:
             return None
         query &= queryLength
 
@@ -36,7 +36,7 @@ def queryString(dict,status):
         if 'maxHeight' in dict :
             query &= Q(height__lte=dict['maxHeight'])
         
-        if dict['minHeight'] and dict['maxHeight'] and dict['minHeight'] > dict['maxHeight']:
+        if 'minHeight' in dict and 'maxHeight' in dict and dict['minHeight'] > dict['maxHeight']:
             return None
         query &= queryLength
 
@@ -48,7 +48,7 @@ def queryString(dict,status):
         if 'maxArea'in dict :
             query &= Q(area__lte=dict['maxArea'])
             
-        if dict['minArea'] and dict['maxArea'] and dict['minArea'] > dict['maxArea']:
+        if 'minArea' in dict and 'maxArea' in dict and dict['minArea'] > dict['maxArea']:
             return None
         query &= queryLength
 
@@ -60,7 +60,7 @@ def queryString(dict,status):
         if 'maxVolume' in dict:
             query &= Q(area__lte=dict['maxVolume'])
 
-        if dict['minVolume'] and dict['maxVolume'] and dict['minVolume'] > dict['maxVolume']:
+        if 'minVolume' in dict and 'maxVolume' in dict and dict['minVolume'] > dict['maxVolume']:
             return None
         query &= queryLength
 
@@ -76,7 +76,7 @@ def queryString(dict,status):
                 query &= Q(created_at__lte=endDate)
             query &= queryLength
 
-            if startDate and endDate and startDate > endDate:
+            if 'startDate' in dict and 'endDate' in dict and startDate > endDate:
                 return None
         if 'createdBy' in dict:
             query &= Q(created_by=dict['createdBy'])
