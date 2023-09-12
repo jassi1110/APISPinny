0b1618db05ba533399ba5264e696218348900244
jassi1110 <jasnoorsingh11@gmail.com>
2023-09-12 17:11:54 +0530

    Add gunicorn in requirement file : Jasnoor

--- api_Service/settings.py
+++ api_Service/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-zpckpdb9q%%x41l3hou1j%hi54^#rtkv-@xyr81eke1!j0t$i8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 AUTH_USER_MODEL = 'inventory.CustomUser'
 
 # Application definition
